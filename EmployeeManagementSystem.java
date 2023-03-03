ba8ede68d43d1f04b6a113d78a80c7b75e72ea3b
TERZAKII <61076947+TERZAKII@users.noreply.github.com>
2023-03-03 16:03:04 +0600

    Rename fileName

--- EmployeeManagementSystem.java
+++ EmployeeManagementSystem.java
@@ -7,7 +7,7 @@ import java.util.*;
 import java.util.regex.Pattern;
 
 
-public class EmployManagementSystem extends MainMenu {
+public class EmployeeManagementSystem extends MainMenu {
     public static void main(String[] args) throws Exception {
         try {
             MainMenu menu = new MainMenu();
